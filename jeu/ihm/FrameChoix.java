a358fb1af632dcb2c818e810835f799e363015ac
w0rrAPs-was-taken <66080788+w0rrAPs-was-taken@users.noreply.github.com>
2024-06-11 15:32:33 +0200

    javadoc

--- jeu/ihm/FrameChoix.java
+++ jeu/ihm/FrameChoix.java
@@ -4,7 +4,18 @@ import jeu.Controleur;
 import jeu.ihm.*;
 import javax.swing.*;
 
-
+/**
+ * Cette classe créé l'interface graphique gérée par le controleur.
+ * Elle s'occupe de charger des éléments sur la page graphique
+ * @author Antione Paunet,			IUT du Havre
+ * @author Mael Vauthier,			IUT du Havre
+ * @author Martin Ravenel,			IUT du Havre
+ * @author Fanch EVEN,				IUT du Havre
+ * @author Nahel KOCHAT,			IUT du Havre
+ * @author Anas AARAB Vauthier,		IUT du Havre
+ * @author Louis THOMAZEAU-AGUILLO,	IUT du Havre
+ * @version 1.0 , 2024-05-23
+ */
 public class FrameChoix extends JFrame
 {
 	private PanelChoixJoueur panelChoixJoueur;
--- jeu/ihm/FrameJeu.java
+++ jeu/ihm/FrameJeu.java
@@ -3,6 +3,18 @@ package jeu.ihm;
 import javax.swing.*;
 import java.awt.event.*;
 
+/**
+ * Cette classe créé l'interface graphique gérée par le controleur.
+ * Elle s'occupe de charger des éléments sur la page graphique
+ * @author Antione Paunet,			IUT du Havre
+ * @author Mael Vauthier,			IUT du Havre
+ * @author Martin Ravenel,			IUT du Havre
+ * @author Fanch EVEN,				IUT du Havre
+ * @author Nahel KOCHAT,			IUT du Havre
+ * @author Anas AARAB Vauthier,		IUT du Havre
+ * @author Louis THOMAZEAU-AGUILLO,	IUT du Havre
+ * @version 1.0 , 2024-05-23
+ */
 public class FrameJeu extends JFrame
 {
 	private PanelCarte panelCarte;
