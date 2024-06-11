e9def17b695b5a36008d42e35f89660abd62100a
Nahel <146019267+SpeakersToo@users.noreply.github.com>
2024-06-11 15:41:43 +0200

    Fix javadoc

--- jeu/metier/IRessource.java
+++ jeu/metier/IRessource.java
@@ -2,12 +2,12 @@ package jeu.metier;
 /**
  * Cette classe créé l'interface graphique gérée par le contrôleur.
  * Elle s'occupe de charger des éléments sur la page graphique
- * @author Antione Paunet,			    IUT du Havre
- * @author Mael Vauthier ,			    IUT du Havre
- * @author Martin Ravenel,			    IUT du Havre
- * @author Fanch EVEN,				      IUT du Havre
- * @author Nahel KOCHAT,			      IUT du Havre
- * @author Anas AARAB,		          IUT du Havre
+ * @author Antoine Paunet,          IUT du Havre
+ * @author Maël Vauthier,           IUT du Havre
+ * @author Martin Ravenel,          IUT du Havre
+ * @author Fanch EVEN,              IUT du Havre
+ * @author Nahel KOCHAT,            IUT du Havre
+ * @author Anas AARAB,              IUT du Havre
  * @author Louis THOMAZEAU-AGULLO,  IUT du Havre
  * @version 1.0 , 2024-06-11
  */
