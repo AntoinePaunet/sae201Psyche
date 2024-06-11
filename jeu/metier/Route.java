14d3eec70350a89c7a1d61e9df697004b5630c90
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2024-06-11 11:21:38 +0200

    Maj Route

--- jeu/metier/Route.java
+++ jeu/metier/Route.java
@@ -1,1 +1,24 @@
 package jeu.metier;
+
+public class Route
+{
+	private Mine mineDep;
+	private Mine mineArr;
+	private int  nbTroncons;
+
+	public Route ( Mine mineDep, Mine mineArr, int nbTroncons  )
+	{
+		this.mineDep    = mineDep;
+		this.mineArr    = mineArr;
+		this.nbTroncons = nbTroncons;
+	}
+
+	public Mine getMineDep   () { return mineDep;    }
+	public int  getNbTroncons() { return nbTroncons; }
+	public Mine getMineArr   () { return mineArr;    }
+
+	public void setMineDep   ( Mine mineDep   ) { this.mineDep = mineDep;       }
+	public void setMineArr   ( Mine mineArr   ) { this.mineArr = mineArr;       }
+	public void setNbTroncons( int nbTroncons ) { this.nbTroncons = nbTroncons; }
+
+}
