41efaea1a0df19900db743910edb32108f83e5fb
Fanch Even <fanch.even@etu.univ-lehavre.fr>
2024-06-11 13:33:06 +0200

    ajout score

--- jeu/metier/Joueur.java
+++ jeu/metier/Joueur.java
@@ -1,25 +1,22 @@
+package jeu.metier;
+
 public class Joueur
 {
 
-	private FrameJoueur plateauIndividuel;
+	/*private FrameJoueur plateauIndividuel;*/
 
 	private String nomJoueur;
 
-	public Joueur(FrameJoueur plateauIndividuel, String nomJoueur)
+	public Joueur(/*FrameJoueur plateauIndividuel,*/ String nomJoueur)
 	{
 
-		this.plateauIndividuel = plateauIndividuel;
+		/*this.plateauIndividuel = plateauIndividuel;*/
 		this.nomJoueur = nomJoueur;
 
 	}
 
 	public String getNomJoueur        () { return this.nomJoueur        ; }
-	public String getPlateauIndividuel() { return this.plateauIndividuel; }
+	/*public String getPlateauIndividuel() { return this.plateauIndividuel; }*/
 	
-
-
-
-
-
 
 }
--- jeu/metier/Plateau.java
+++ jeu/metier/Plateau.java
@@ -5,6 +5,9 @@ public class Plateau
 	private Materiaux tabPlateau[][];
 	private Materiaux tabPiece[];
 
+	private int score ;
+	private String detailScore;
+
 	public Plateau()
 	{
 		this.tabPlateau = new Materiaux [4][8];
@@ -55,5 +58,70 @@ public class Plateau
 			}
 		}
 		return false;
+	}
+
+	public void score()
+	{
+		int[] scoresCol = {20,10,0,2           };
+		int[] scoresLig = {0,4,9,16,25,36,49,64};
+		//Affichage des détails
+		int score, scoreMonnaie, scoreCol, scoreLig;
+		String detail = "Detail :\n ";
+
+
+		//Compteur pour le score des Monnaies
+		scoreMonnaie = 0;
+		for (int i = 1; i < this.tabPiece.length; i++)
+		{
+			if (this.tabPiece[i] != null)
+				scoreMonnaie = (i+1)*(i+1);
+		}
+
+		detail += "Monnaies      : " + scoreMonnaie + " pt \n ";
+
+		score = scoreMonnaie;
+
+
+		//Compteur pour le score des colonnes
+		int cptCol = 0;
+		
+		while (cptCol < this.tabPlateau[0].length) // 0 - 7
+			for (int i = 0 ; i < this.tabPlateau.length; i++) // 0 - 3
+			{
+				scoreCol = 0;
+				if  (this.tabPlateau[i][cptCol] != null)
+					scoreCol=scoresLig[i];
+				detail += "Colonne " + (cptCol+1) + "   : " + String.format("%2d", scoreCol) + " pt\n ";
+				score += scoreCol;
+
+				cptCol++;
+			}
+
+
+		//Compteur pour le score des lignes
+		int cptRessource= 0;
+		int cptLig 		= 1;
+		int cptPieceLig = 0;
+		
+
+		for(int i = 0; i < this.tabPlateau.length ; i++) //0 - 3
+		{
+			scoreLig = 0;
+			for(int j = 0 ; j < this.tabPlateau[i].length ; j++) //0 - 7
+			{
+				if(this.tabPlateau[i][j] != null)
+					cptRessource++;
+			}
+
+			scoreLig += scoresLig[cptRessource];
+			detail += "Ligne   " + (i + 1) + "   : " + String.format("%2d", scoreLig) + " pt\n ";
+			score  += scoreLig;
+
+			cptRessource = 0;
+		}
+
+		this.score = score;
+		this.detailScore = detail;
+
 	}
 }
