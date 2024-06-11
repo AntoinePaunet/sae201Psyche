9dad9020e9616cdcc55fd1badb7e56eb3b118ede
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2024-06-11 13:41:49 +0200

    maj route

--- jeu/metier/Route.java
+++ jeu/metier/Route.java
@@ -2,23 +2,19 @@ package jeu.metier;
 
 public class Route
 {
-	private Mine mineDep;
-	private Mine mineArr;
-	private int  nbTroncons;
+	private Mine mineSommet;
+	private int  mineSection;
 
-	public Route ( Mine mineDep, Mine mineArr, int nbTroncons  )
+	public Route ( Mine mineSommet, int mineSection )
 	{
-		this.mineDep    = mineDep;
-		this.mineArr    = mineArr;
-		this.nbTroncons = nbTroncons;
+		this.mineSommet  = mineSommet;
+		this.mineSection = mineSection;
 	}
 
-	public Mine getMineDep   () { return mineDep;    }
-	public int  getNbTroncons() { return nbTroncons; }
-	public Mine getMineArr   () { return mineArr;    }
+	public Mine getMineSommet  () { return this.mineSommet;  }
+	public int  getMineSection () { return this.mineSection; }
 
-	public void setMineDep   ( Mine mineDep   ) { this.mineDep = mineDep;       }
-	public void setMineArr   ( Mine mineArr   ) { this.mineArr = mineArr;       }
-	public void setNbTroncons( int nbTroncons ) { this.nbTroncons = nbTroncons; }
+	public void setMineSommet  ( Mine mineSommet  ) { this.mineSommet = mineSommet;   }
+	public void setMineSection ( int  mineSection ) { this.mineSection = mineSection; }
 
 }
