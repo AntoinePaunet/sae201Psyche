17db249aa193b8a48df427bdd244eeedc372e6b5
w0rrAPs-was-taken <66080788+w0rrAPs-was-taken@users.noreply.github.com>
2024-06-12 09:38:11 +0200

    dvswdfgv

--- jeu/ihm/FrameDemarrage.java
+++ jeu/ihm/FrameDemarrage.java
@@ -7,6 +7,16 @@ import jeu.Controleur;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+ * Cette classe créé l'interface graphique gérée par le controleur.
+ * Elle s'occupe de charger des éléments sur la page graphique
+ * @author Antione Paunet,			IUT du Havre
+ * @author Mael Vauthier,			IUT du Havre
+ * @author Martin Ravenel,			IUT du Havre
+ * @author Fanch EVEN,				IUT du Havre
+ * @author Anas AARAB,				IUT du Havre
+ * @version 1.0 , 2024-05-23
+ */
 public class FrameDemarrage extends JFrame implements ActionListener
 {
 	private Controleur ctrl;
