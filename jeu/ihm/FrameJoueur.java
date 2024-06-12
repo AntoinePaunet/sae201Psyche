de0322bd25bc8da1abe5545531e27b0775c66fdc
Antoine PAUNET <antoine.paunet@etu.univ-lehavre.fr>
2024-06-12 09:56:53 +0200

    t

--- jeu/ihm/FrameJoueur.java
+++ jeu/ihm/FrameJoueur.java
@@ -104,7 +104,7 @@ public class FrameJoueur extends JFrame
      */
     public void ajoutImage(int x, int y, String url, int layer)
     {
-        ImageIcon image = new ImageIcon(getClass().getResource("images/" + url));
+        ImageIcon image = new ImageIcon(getClass().getResource("class/jeu/ihm/images/" + url));
         JLabel imgLabel = new JLabel(image);
         imgLabel.setBounds(x, y, image.getIconWidth(), image.getIconHeight());
         this.panelFond.add(imgLabel, Integer.valueOf(layer));
